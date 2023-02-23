7be1e63904efedf2d13bb5fe171175cb0ad58ec4
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-23 09:51:50 +0500

    1

--- modani_app/models.py
+++ modani_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Catolog(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Sub_category(models.Model):
+    name = models.CharField(max_length=200)
+    catolog = models.ForeignKey(Catolog, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    dimensions = models.TextField()
+    img = models.TextField()
+    img_dimensions = models.TextField()
+    features = models.TextField()
+    sub_category = models.ForeignKey(Sub_category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
