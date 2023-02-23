0bc9f9d9e1db4fabd77115ddf2bbecb0e83d224c
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-23 15:04:38 +0500

    models site registor

--- modani_app/admin.py
+++ modani_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Catolog, Sub_category, Product
 # Register your models here.
+admin.site.register(Catolog)
+admin.site.register(Sub_category)
+admin.site.register(Product)
