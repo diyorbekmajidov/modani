6a00b9a9ee3a720cedc91987f8226ae730c96269
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-24 22:28:13 +0500

    sub catolog post

--- modani_app/urls.py
+++ modani_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CatologListView
+from .views import CatologListView,SubcategoryListView
 
 urlpatterns = [
     path('catologs/', CatologListView.as_view()),
+    path('sub_catogry/', SubcategoryListView.as_view())
 ]
