867e73ef58fe6bf25defac6d3a49744b16b94645
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-24 22:25:37 +0500

    update

--- modani_app/serialzers.py
+++ modani_app/serialzers.py
@@ -9,11 +9,9 @@ class CatologSerializer(serializers.ModelSerializer):
 class Sub_categorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Sub_category
-        name = serializers.PrimaryKeyRelatedField(queryset=Catolog.objects.all())
         fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        name = serializers.PrimaryKeyRelatedField(queryset=Sub_category.objects.all())
         fields = '__all__'
