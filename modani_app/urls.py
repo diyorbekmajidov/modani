eea3ad8e9dde0430e4fd3bc5b89ed803dd2adf0c
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-25 14:17:52 +0500

    add products url

--- modani_app/urls.py
+++ modani_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CatologListView,SubcategoryListView
+from .views import CatologListView,SubcategoryListView,Subcategoryget,ProductListView
 
 urlpatterns = [
     path('catologs/', CatologListView.as_view()),
-    path('sub_catogry/', SubcategoryListView.as_view())
+    path('sub_catogry/', SubcategoryListView.as_view()),
+    path('sub_catogryget/<int:id>/', Subcategoryget.as_view()),
+    path('products/', ProductListView.as_view()),
 ]
