bf9353a57d6bcd6258d7ceb21c82c5ce1c1d3726
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-25 14:37:36 +0500

    getcatolog

--- modani_app/urls.py
+++ modani_app/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import CatologListView,SubcategoryListView,Subcategoryget,ProductListView
+from .views import (
+    CatologListView,
+    SubcategoryListView,
+    Subcategoryget,
+    ProductListView,
+    Productget,
+    Getcatolog
+)
 
 urlpatterns = [
     path('catologs/', CatologListView.as_view()),
     path('sub_catogry/', SubcategoryListView.as_view()),
     path('sub_catogryget/<int:id>/', Subcategoryget.as_view()),
     path('products/', ProductListView.as_view()),
+    path('getproduct/<int:id>/', Productget.as_view()),
+    path('getcatolog', Getcatolog.as_view()),
 ]
