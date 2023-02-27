65d37b54cbde67c0389fb35d64249971aa3a9700
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-27 09:12:29 +0500

    delet product

--- modani_app/urls.py
+++ modani_app/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from .views import (
     CatologListView,
-    SubcategoryListView,
-    Subcategoryget,
-    ProductListView,
-    Productget,
     Getcatolog,
     Updatecatolog,
-    Deletecatolog
+    Deletecatolog,
+    SubcategoryListView,
+    Subcategoryget,
+    Subcatogorydelet,
+    UpdateSubcategory,
+    ProductListView,
+    Productget,
+    Productdelet,
+    UpdateProduct
 )
 
 urlpatterns = [
@@ -16,7 +20,11 @@ urlpatterns = [
     path('sub_catogryget/<int:id>/', Subcategoryget.as_view()),
     path('products/', ProductListView.as_view()),
     path('getproduct/<int:id>/', Productget.as_view()),
-    path('getcatolog', Getcatolog.as_view()),
+    path('getcatolog/', Getcatolog.as_view()),
     path('updatecatolog/<int:id>/', Updatecatolog.as_view()),
     path('deletcatolog/<int:id>/', Deletecatolog.as_view()),
+    path('updatesubcatogry/<int:id>/', UpdateSubcategory.as_view()),
+    path('deletsubcatogry/<int:id>/', Subcatogorydelet.as_view()),
+    path('updateproduct/<int:id>/', UpdateProduct.as_view()),
+    path('deletproduct/<int:id>/', Productdelet.as_view()),
 ]
