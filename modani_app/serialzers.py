c60c2fc08a2f1846480a29b98559f94f26034551
Diyorbek <majidovdiyorbek741@gmail.com>
2023-02-28 17:57:22 +0500

    1

--- modani_app/serialzers.py
+++ modani_app/serialzers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from .models import Product, Sub_category, Catolog
 
-class CatologSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Catolog
-        fields = '__all__'
-
 class Sub_categorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Sub_category
+        fields = '__all__'
+
+class CatologSerializer(serializers.ModelSerializer):
+    sub_category = Sub_categorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Catolog
         fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
