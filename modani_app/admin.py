03553b1e8ac6dab6f931b1dd22e9adb3a0ff6e79
Diyorbek <majidovdiyorbek741@gmail.com>
2023-03-04 11:15:34 +0500

    cart sale like

--- modani_app/admin.py
+++ modani_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Catolog, Sub_category, Product
+from .models import Catolog, Sub_category, Product, Sale, Cart, Like
 # Register your models here.
 admin.site.register(Catolog)
 admin.site.register(Sub_category)
 admin.site.register(Product)
+admin.site.register(Sale)
+admin.site.register(Cart)
+admin.site.register(Like)
