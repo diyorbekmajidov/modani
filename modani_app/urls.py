3d0ad35662dbcc6434d5cd677946657f64c89494
Diyorbek <majidovdiyorbek741@gmail.com>
2023-03-04 11:16:47 +0500

    catlog add

--- modani_app/urls.py
+++ modani_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    # CatologListView,
+    CatologListView,
     Getcatolog,
     Updatecatolog,
     Deletecatolog,
@@ -13,22 +13,24 @@ from .views import (
     Productdelet,
     UpdateProduct,
     SubCategory,
-    CatologView,
+
 )
 
 urlpatterns = [
-    # path('catologs/', CatologListView.as_view()),
-    path('sub_catogry/', SubcategoryListView.as_view()),
-    path('sub_catogryget/<int:id>/', Subcategoryget.as_view()),
-    path('products/', ProductListView.as_view()),
-    path('getproduct/<int:id>/', Productget.as_view()),
+    path('catologs/', CatologListView.as_view()),
     path('getcatolog/', Getcatolog.as_view()),
-    path('updatecatolog/<int:id>/', Updatecatolog.as_view()),
     path('deletcatolog/<int:id>/', Deletecatolog.as_view()),
+    path('updatecatolog/<int:id>/', Updatecatolog.as_view()),
+
+    path('sub_catogry/', SubcategoryListView.as_view()),
     path('updatesubcatogry/<int:id>/', UpdateSubcategory.as_view()),
     path('deletsubcatogry/<int:id>/', Subcatogorydelet.as_view()),
+    path('sub_catogryget/<int:id>/', Subcategoryget.as_view()),
+
+    path('products/', ProductListView.as_view()),
+    path('getproduct/<int:id>/', Productget.as_view()),
     path('updateproduct/<int:id>/', UpdateProduct.as_view()),
     path('deletproduct/<int:id>/', Productdelet.as_view()),
+    
     path('subcatogry/<int:pk>/', SubCategory.as_view()),
-    path('catolog/', CatologView.as_view()),
 ]
